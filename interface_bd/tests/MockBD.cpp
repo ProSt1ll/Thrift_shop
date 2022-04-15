0a98087da0c3bed6542eeba2d6c8258dfa0d0601
Mike5535 <volkovnhk@mail.ru>
2022-04-16 00:11:31 +0300

    second

--- interface_bd/tests/MockBD.cpp
+++ interface_bd/tests/MockBD.cpp
@@ -1,17 +1,8 @@
-#include "../headers/interface_BD.h"
 #include "MockBD.h"
 
 using namespace bd;
 
-TEST(TestBI, construstor)
-{
-    EXPECT_CALL(basedata, MockBI())                
-      .Times(AtLeast(1));
-
-    MockBI basedata;
-}
-
 int main() {
-    testing::InitGoogleTest();
-    return RUN_ALL_TESTS();
+    /*testing::InitGoogleTest();
+    return RUN_ALL_TESTS();*/
 }
--- interface_bd/tests/MockBD.h
+++ interface_bd/tests/MockBD.h
@@ -1,4 +1,5 @@
 #include <gmock/gmock.h>
+#include <string>
 #include "../headers/interface_BD.h"
 
 using namespace bd;
@@ -7,17 +8,14 @@ class MockBI : public BI {
 public:
     MockBI() : BI() {}
 
-    MOCK_METHOD(void, set_product, (Categories, Product));
-    MOCK_METHOD(void, set_price, (Categories, std::string, float));
+    MOCK_METHOD(void,set_product,(Categories, Product));
+    MOCK_METHOD(void,set_price,(Categories, std::string, float));
+    MOCK_METHOD(void,set_url_image,(Categories, std::string, std::string));
+    MOCK_METHOD(void,set_url_product,(Categories, std::string, std::string));
+    MOCK_METHOD(void,set_parametrs,(Categories, std::string, Parametrs));
 
-    MOCK_METHOD(void, set_product, (Categories, Product));
-    MOCK_METHOD(void, set_price, (Categories, std::string, float));
-    MOCK_METHOD(void, set_url_image, (Categories, std::string, std::string));
-    MOCK_METHOD(void, set_url_product, (Categories, std::string, std::string));
-    MOCK_METHOD(void, set_parametrs, (Categories, std::string, Parametrs));
-
-    MOCK_METHOD(product*, get_products, (Categories));
-    MOCK_METHOD(product*, search_price, (Categories, float));
-    MOCK_METHOD(product*, search_parametrs, (Categories, Parametrs));
+    MOCK_METHOD(Product*,get_products,(Categories));
+    MOCK_METHOD(Product*,search_price,(Categories, float));
+    MOCK_METHOD(Product*,search_parametrs,(Categories, Parametrs));
 
 };

f6b7ddaa025aac64ac8d33f7632fe5923394db68
ProSt1ll <gubilnurr@gmail.com>
2022-04-16 03:42:19 +0300

    delete trash

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ilnur/Technopark/Thrift_shop
-# Build directory: /home/ilnur/Technopark/Thrift_shop/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
